7d59c42fdb807529b3ff5ba4f5e9596996e89717
Shahman <magomadovshahman@gmail.com>
2022-05-12 16:44:21 +0300

    video_11

---
+++ src/main/java/com/example/sweater/config/BeanConfig.java
@@ -0,0 +1,14 @@
+package com.example.sweater.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BeanConfig {
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder(8);
+	}
+}
